5071e75648c96b00b3b9f5652c5507d91db2859d
Dálcio Garcia <dalciomacuetegarcia@gmail.com>
2021-06-17 13:01:55 +0100

    feat: audio last changes

--- src/components/audio/record-button/index.tsx
+++ src/components/audio/record-button/index.tsx
@@ -1,25 +1,41 @@
 import React, { FC } from 'react';
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder';
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { useApp } from '@app-data';
 
 import { AudioProps } from '../types';
 import { RecordContainer } from './styled';
+import { useState } from 'react';
 
 export const RecordButton: FC<AudioProps> = ({ setShow, show, setStep }) => {
+    const [recordState, setRecordState] = useState<string>();
     const { setAudioFile } = useApp();
 
-    const onStartRecord = () => {
+    const start = () => {
         setShow('record');
+        setRecordState(RecordState.START);
     };
 
-    const onStopRecord = () => {
+    const stop = () => {
+        setRecordState(RecordState.STOP);
+    };
+
+    const onStopRecord = (audioData) => {
         setStep(2);
+        console.log('audioData', audioData);
     };
 
     return (
-        <RecordContainer onClick={onStartRecord} show={show === 'record'}>
-            <FontAwesomeIcon icon={faMicrophone} id="text" />
-        </RecordContainer>
+        <>
+            <AudioReactRecorder state={recordState} onStop={onStopRecord} />
+            <RecordContainer
+                onClick={(show === 'record' && stop) || start}
+                show={show === 'record'}
+            >
+                <FontAwesomeIcon icon={faMicrophone} id="text" />
+            </RecordContainer>
+        </>
     );
 };
